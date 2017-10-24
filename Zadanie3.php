61077a2bc8f7e04e8ac290c32ca9b78f821c888f
ms08 <michal.stanek@me.com>
2017-10-24 23:15:41 +0200

    zadanie3

--- Zadanie3.php
+++ Zadanie3.php
@@ -1,5 +1,42 @@
 <?php
 
-class ShowArticles{
+$file = fopen("products.csv", "r");
+if($file){
+    $firstLine = fgets($file);
+    $parser = new CSVParser();
+    $parser->getColumns($firstLine);
+
+    while(($line = fgets($file)) !== false){
+        $parser->parseLine($line);
+    }
+
+    echo("<pre>");
+    var_dump($parser->getQueries());
+    echo("</pre>");
+    fclose($file);
+}
+
+class CSVParser {
+    private $columnsString;
+    private $queries;
+
+    public function getColumns($firstLine){
+        $this->columnsString = str_replace(";",",",$firstLine);
+    }
+    public function parseLine($line){
+        $values = explode(";",$line);
+        for($i = 0; $i<count($values);$i++){
+            $values[$i] = "'".$values[$i]."'";
+        }
+
+        $mysqlLine = implode(",",$values);
+
+        $this->queries[] = "INSERT INTO table($this->columnsString) VALUES($mysqlLine);";
+
+    }
+
+    public function getQueries(){
+        return $this->queries;
+    }
 
 }
